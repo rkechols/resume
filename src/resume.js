2fe70710ca28c78ac1829bf9af4b39fccf72b96e
Ryan Echols <rkechols21@gmail.com>
2020-02-17 16:03:00 -0700

    filled in majority of resume

--- src/resume.js
+++ src/resume.js
@@ -4,8 +4,11 @@ const email = 'rkechols21@gmail.com'
 const fullName = 'Ryan Echols'
 const gitHubUsername = 'rkechols'
 const linkedInURL = 'ryan-echols-750'
+const bitbucketUsername = 'echols14'
 const majorAndMinors = 'BA in Linguistics, minors in Computer Science and Mathematics'
-const gpa = '3.65'
+const gpa = '3.66'
+const currentYear = 2020
+const startedProgrammingYear = 2013
 
 // JSX is supported in most places in this template in place of strings
 const resume = {
@@ -22,6 +25,11 @@ const resume = {
         icon: 'fab fa-github',
         text: `github.com/${gitHubUsername}`,
       },
+	  {
+        href: `https://bitbucket.org/${bitbucketUsername}/profile/repositories`,
+        icon: 'fab fa-bitbucket',
+        text: `bitbucket.org/${bitbucketUsername}/profile/repositories`,
+      },
       {
         href: `https://linkedin.com/in/${linkedInURL}`,
         icon: 'fab fa-linkedin',
@@ -30,25 +38,37 @@ const resume = {
     ],
   },
   summary: [
-    'Seeking {job type} position as a {position type} starting {month} {year}',
+    'Seeking summer internship in machine learning or computational linguistics starting May 2020',
     `Graduating December 2021 with a ${majorAndMinors}`,
-    '6 years of programming experience',
-    '{current position} at {current company}',
-    'Skills: C++, Java, Python, Git, Advanced Mathematics',
-	'Foreign Languages: Serbian, Bosnian, Croatian, Slovene, Spanish'
+    `${currentYear - startedProgrammingYear} years of programming experience`,
+    'Computer Programmer at BYU Office of Digital Humanities',
   ],
   sections: [
+	{
+      title: 'Skills',
+      subsections: [
+        {
+          details: [
+			'Computer Science: Java, C++, Python, Kotlin, Android, Git, command-line',
+            'Advanced Foreign Languages: Serbian, Bosnian, Croatian, Slovene',
+			'Novice Foreign Languages: Spanish, German, Russian',
+			'Advanced Mathematics: Linear Algebra, Mutlivariable Calculus, Ordinary Differential Equations'
+          ]
+        }
+      ]
+    },
     {
       title: 'Education',
       subsections: [
         {
-          title: <a href="{university homepage}" target="_blank" rel="noopener noreferrer">{'Brigham Young University'}</a>,
+          title: 'Brigham Young University',
           subtitle: 'Senior - Expected Graduation Date: December 2021',
           details: [
             majorAndMinors,
             `Current GPA: ${gpa}`,
-            'Completed Coursework: {class names of most notable classes}',
-            'Activities: {club or other involvement}'
+            'Completed Coursework: Structure in Language, Sounds in Language, Intro to Computational Theory, \
+			Principles of Statistics, Advanced Programming Concepts, Algorithm Design and Optimization, Discrete Structure, Linear Algebra, Calculus',
+            'Extracurricular : Association for Computing Machinery, BYU Student Association, Celtic Folk Ensemble, Country Dance Club'
           ]
         }
       ]
@@ -56,32 +76,69 @@ const resume = {
     {
       title: 'Experience',
       subsections: [
-        {
-          title: <a href="factory.hr" target="_blank" rel="noopener noreferrer">{'Plava tvornica ("Blue Factory") - Osijek, Croatia'}</a>,
-          subtitle: 'Android Development Intern - May 2019 to July 2019',
+		{
+          title: 'NLP Computer Programmer',
+          subtitle: 'BYU Office of Digital Humanities - March 2019 to present',
           details: [
-            '2-3 verb-driven bullet points that highlight achievements in the form of quantifiable results',
-            'Major technologies utilized during the project',
+            'Implement models to analyze and categorize Russian text',
+            'Maintain functioning multi-branch Git repository with multiple developers',
           ]
         },
 		{
-          title: <a href="{company homepage}" target="_blank" rel="noopener noreferrer">{'company name'}</a>,
-          subtitle: '{position} - {start date} to {end date}',
+          title: 'Teaching Assistant',
+          subtitle: 'BYU Department of Computer Science - September 2015 to present',
           details: [
-            '2-3 verb-driven bullet points that highlight achievements in the form of quantifiable results',
-            'Major technologies utilized during the project',
+            'Instruct 300+ students each semester in principles of coding and computation',
+            'Guide students to good coding practices and develop debugging skills',
           ]
         },
+        {
+          title: 'Android Development Intern',
+		  subtitle: <a href="https://factory.hr/" target="_blank" rel="noopener noreferrer">{'Plava tvornica ("Blue Factory") - Osijek, Croatia - May 2019 to July 2019'}</a>,
+          details: [
+            'Created 5 functioning mobile applications for Android while self-directed',
+            'Adapted mobile applications to be displayed in 3+ languages each',
+          ]
+        },
+		{
+          title: 'Research Assistant',
+          subtitle: 'BYU Department of Mathematics - February 2015 to April 2016',
+          details: [
+            'Converted mathematical algorithms from theory to Python code',
+            'Extracted and processed data for analysis and visualization to be presented weekly',
+          ]
+        },
+      ]
+    },
+	{
+      title: 'Projects',
+      subsections: [
+        {
+          title: 'Neural Network Java Library',
+          details: [
+            'Created a generic Java library for simple Neural Networks, all from the ground up',
+			'Incorporated stochastic gradient descent and multi-threading for efficiency',
+			'Tested the library on the MNIST dataset, achieving 94.5% accuracy before deliberate adjustment'
+          ]
+        },
+		{
+          title: 'Language Imitator',
+          details: [
+            'Designed and implemented a language imitator based on character transitions, using Markov Chains',
+          ]
+        }
       ]
     },
     {
       title: 'Service',
       subsections: [
         {
-          title: <a href="{organization homepage}" target="_blank" rel="noopener noreferrer">{'organization name'}</a>,
-          subtitle: '{position} - {start date} to {end date}',
+          title: 'Full-time Volunteer',
+          subtitle: 'The Church of Jesus Christ of Latter-day Saints in Serbia, Slovenia, Bosnia & Herzegovina - August 2016 to August 2018',
           details: [
-            '2-3 verb-driven bullet points that highlight achievements in the form of quantifiable results',
+            'Served as an administrative clerk over church records in 5 countries',
+			'Supervised 3 of the church’s official Facebook pages',
+			'Written and spoken translation and interpretation in Serbian, Bosnian, Croatian, and Slovene'
           ]
         },
       ]
